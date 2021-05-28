29c44b0618fb346c10a2ce42235c36984cf67b5a
Georgely Jo Santino <47821682+georgelyjo@users.noreply.github.com>
2021-05-28 13:41:24 +0530

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
 export const baseUrl = 'https://api.themoviedb.org/3';
 
-export const API_KEY = '1fce6d8a14c0f4b051adbcd0182f4679';
+export const API_KEY = '<YOUR-API';
 
 export const imageUrl = 'https://image.tmdb.org/t/p/original';
